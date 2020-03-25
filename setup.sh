b783a86f08d85fc803e9eb64caabd94afff82653
Aravind Reddy V <zerodividedby0@gmail.com>
2020-03-25 13:26:26 +0530

    source after install

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ GITHUBREPO="https://raw.githubusercontent.com/zeroby0/dotfiles/master/src/"
 
 cd $HOME
 curl -fsSLO "$GITHUBREPO/.hyper.js"
+rm -rf $DOTFOLDER
 mkdir $DOTFOLDER && cd $_
 curl -fsSLO "$GITHUBREPO/aliases.sh"
 curl -fsSLO "$GITHUBREPO/funcs.sh"
@@ -10,6 +11,8 @@ curl -fsSLO "$GITHUBREPO/myz.sh"
 curl -fsSLO "$GITHUBREPO/subl"
 
 echo "source $HOME/.dotfiles/myz.sh" >> "$HOME/.zshrc"
+source $HOME/.dotfiles/myz.sh
+
 
 
 
